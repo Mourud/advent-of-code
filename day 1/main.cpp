bd4199a240a14179633a87e3bb4eb69f0740e9e2
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2023-06-07 22:11:28 -0400

    day 1 part 2 complete

--- day 1/main.cpp
+++ day 1/main.cpp
@@ -7,33 +7,52 @@ using namespace std;
 int main()
 {
     string line;
-    ifstream myfile("input.txt");
+    ifstream myfile("day 1/input.txt");
 
     int total = 0;
-    int max = 0;
+    int max[3] = {0};
     int flag = 0;
     while (getline(myfile, line))
     {
-        try
+
+        if (line.empty())
         {
-            int i = std::stoi(line);
-            total += i;
-        }
-        catch (const std::invalid_argument &ia)
-        {
-            if (flag == 0) {
-                max = total;
-                flag =1;
+            if (flag == 0)
+            {
+                max[0] = total;
+                max[1] = total;
+                max[2] = total;
+                flag = 1;
             }
-            if (total > max){
-                max = total;
+            else if (total > max[0])
+            {
+                int temp = max[1];
+                max[1] = max[0];
+                max[0] = total;
+                max[2] = temp;
+            }
+            else if (total > max[1])
+            {
+                int temp = max[1];
+                max[1] = total;
+                max[2] = temp;
+            }
+            else if (total > max[2])
+            {
+                max[2] = total;
             }
             total = 0;
-            cout << max << endl;
-            
         }
-        
+        else
+        {
+            total += stoi(line);
+        }
     }
+    total = max[0] + max[1] + max[2];
+    std::cout << max[0] << endl;
+    std::cout << max[1] << endl;
+    std::cout << max[2] << endl;
+    std::cout << total << endl;
 
     return 0;
 }
