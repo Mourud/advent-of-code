9bec7f78fd077a141c0c8e994b610288845a983d
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-04-04 04:09:53 +0600

    dirty commit

--- day3/p1.cpp
+++ day3/p1.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Options.hpp>
+#include <curlpp/Easy.hpp>
 
-curlpp::Cleanup myCleanup;
+int main() {
+    curlpp::Cleanup myCleanup;
 
-std::ostringstream os;
+    // Create an ostringstream to write the result to.
+    std::ostringstream os;
 
-os << curlpp::options::Url(std::string("http://adventofcode.com/2022/day/3/input"))
+    // Create an instance of curlpp::Easy.
+    curlpp::Easy request;
 
-string input = os.str();
+    // Set the URL option on the request.
+    request.setOpt(new curlpp::options::Url("http://adventofcode.com/2022/day/3/input"));
+    
+    // Set the WriteStream option on the request to write to the ostringstream.
+    request.setOpt(new curlpp::options::WriteStream(&os));
 
-int main(){
-    std::cout << input << etd::endl;
+    // Perform the request.
+    request.perform();
+
+    // Extract the result.
+    std::string result = os.str();
+
+    std::cout << result << std::endl;
+
     return 0;
 }
