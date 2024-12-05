158768b33e073a38e9692798a9206b69bcc3b129
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-12-05 12:43:05 -0800

    first pass at day4

---
+++ day4.py
@@ -0,0 +1,48 @@
+from numpy import mat, matrix
+from input_fetcher import fetch_input
+
+def main():
+    input = fetch_input(__file__)
+    print(part1(input))
+    
+
+def part1(input):
+    matrix_of_chars = input.splitlines()
+    transposed_matrix_of_chars = transpose_matrix(matrix_of_chars)
+    horizonals = find_XMAS_hor(matrix_of_chars)
+    verticals = find_XMAS_hor(transposed_matrix_of_chars)
+    diagoanls = find_XMAS_diagonal(matrix_of_chars)
+    return horizonals + verticals + diagoanls
+
+def find_XMAS_diagonal(matrix):
+    total = 0
+    for i in range(len(matrix) - 4):
+        for j in range(len(matrix[0]) - 4):
+            X = matrix[i][j]
+            M = matrix[i+1][j+1]
+            A = matrix[i+2][j+2]
+            S = matrix[i+3][j+3]
+            if is_diagonal_xmas(X,M,A,S):
+                total +=1
+            
+
+    return total
+
+def is_diagonal_xmas(X, M, A, S):
+    return (X == 'X' and M == 'M' and A == 'A' and S == 'S') or (S == 'X' and A == 'M' and M == 'A' and X == 'S')
+
+
+def find_XMAS_hor(matrix):
+    return sum([line.count('XMAS') + line.count('SAMX') for line in matrix])
+
+def transpose_matrix(matrix):
+    transpose = [[0 for _ in line] for line in matrix]
+    for i, line in enumerate(matrix):
+        for j, c in enumerate(line):
+            transpose[j][i] = c
+    for line in transpose:
+        line = ''.join(line)
+            
+    return transpose
+
+main()
