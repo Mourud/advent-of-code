194f0eb8cfa5855ef810021907666a91ca734d80
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-12-10 16:24:43 -0800

    added a parse function to split the information

--- 2024/day5.py
+++ 2024/day5.py
@@ -2,12 +2,29 @@ from input_fetcher import fetch_input
 
 
 def main():
-    input = fetch_input()
-    print(part1(input))
-    print(part2(input))
+    input = fetch_input(__file__)
+    print(input)
+    parsed_input = parse_input(input)
+    print(part1(parsed_input))
+    print(part2(parsed_input))
+
+def parse_input(input):
+    rules = []
+    pages = []
+    combined_list = [rules, pages]
+    i = 0
+    for line in input.splitlines():
+        if line == "":
+            i += 1
+        combined_list[i].append(line)
+    return combined_list
 
 def part1(input):
+    
     pass
 
 def part2(input):
     pass
+
+
+main()
