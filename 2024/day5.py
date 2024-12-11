c1ae4ed85ef8ab7e35a50cde415a6f076499229f
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-12-10 16:14:25 -0800

    added day5 template

---
+++ 2024/day5.py
@@ -0,0 +1,13 @@
+from input_fetcher import fetch_input
+
+
+def main():
+    input = fetch_input()
+    print(part1(input))
+    print(part2(input))
+
+def part1(input):
+    pass
+
+def part2(input):
+    pass
