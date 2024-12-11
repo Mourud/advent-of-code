7279afa229884f33765ed2a281f7869a32c9a6a4
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-12-11 11:35:41 -0800

    reduced request to api

--- 2024/input_fetcher.py
+++ 2024/input_fetcher.py
@@ -3,12 +3,18 @@ from pathlib import Path
 import requests
 
 def fetch_input(filename):
+
     stripped_filename = os.path.splitext(os.path.basename(filename))[0]
     year = os.path.basename(Path(filename).parent)
     day = stripped_filename.split("day")[1]
     directory = Path("inputs")
-    directory.mkdir(parents = True, exist_ok= True)
     input_path = directory / f"day{day}.txt"
+
+    if input_path.exists():
+        with open(input_path, "r") as file:
+            return file.read()
+        
+    directory.mkdir(parents = True, exist_ok= True)
     url = f"https://adventofcode.com/{year}/day/{day}/input"
     cookie = os.getenv("AOC_cookie")
     headers = {"Cookie" : f"session={cookie}",
