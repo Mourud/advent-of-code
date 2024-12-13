3c90ee2b36a943e15d3990f10c46520f7f8cef74
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-12-12 23:36:39 -0800

    day5 part1 complete
    fought the urge to optimize in order to find a solution

--- 2024/day5.py
+++ 2024/day5.py
@@ -1,27 +1,45 @@
-from input_fetcher import fetch_input
-
+from aoc_input_fetcher import fetch_input
+from bintrees import AVLTree
 
 def main():
     input = fetch_input(__file__)
-    print(input)
     parsed_input = parse_input(input)
     print(part1(parsed_input))
     print(part2(parsed_input))
 
 def parse_input(input):
-    rules = []
-    pages = []
-    combined_list = [rules, pages]
+    combined_list = [[], []]
     i = 0
     for line in input.splitlines():
         if line == "":
             i += 1
+            continue
         combined_list[i].append(line)
     return combined_list
 
 def part1(input):
-    
-    pass
+    num_map = {}
+    for line in input[0]:
+        nums = [int(x) for x in line.split('|')]
+        num_map.setdefault(nums[1], set())
+        num_map[nums[1]].add(nums[0])
+    total = 0
+    for line in input[1]:
+        list_of_nums = [int(x) for x in line.split(',')]
+        is_rules_broken = False
+        for i, num in enumerate(list_of_nums):
+            if num in num_map.keys():
+                rules = num_map[num]
+                print(num, rules, list_of_nums[i+1:])
+                for rule_num in list_of_nums[i+1:]:
+                    if rule_num in rules:
+                        is_rules_broken = True
+                        break
+                
+        if not is_rules_broken:
+            total += list_of_nums[int(len(list_of_nums)/2)]
+    return total
+
 
 def part2(input):
     pass
