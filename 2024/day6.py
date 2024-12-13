2c29b3d383f246fb706f80c884303c57c9d6c1f0
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-12-13 01:03:39 -0800

    day6 template added

---
+++ 2024/day6.py
@@ -0,0 +1,15 @@
+from aoc_input_fetcher import fetch_input
+
+
+def main():
+    input = fetch_input(__file__)
+    print(part1(input))
+    print(part2(input))
+
+def part1(input):
+    pass
+
+def part2(input):
+    pass
+
+main()
