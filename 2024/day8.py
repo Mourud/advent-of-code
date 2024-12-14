23456dcc967b74ce2fa5bdcea97262b3b525eafe
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-12-13 17:30:44 -0800

    day 8 template

---
+++ 2024/day8.py
@@ -0,0 +1,19 @@
+from aoc_input_fetcher import fetch_input
+
+def main():
+    data = fetch_input(__file__)
+    print(data)
+    data = parse_data(data)
+    print(part1(data))
+    print(part2(data))
+
+def parse_data(data):
+    pass
+
+def part1(data):
+    pass
+    
+def part2(data):
+    pass
+
+main()
