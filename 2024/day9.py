4638cd3a69af79686e6354814addba784e3b84fb
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-12-14 15:26:48 -0800

    part 1 complete
    required some debugging

--- 2024/day9.py
+++ 2024/day9.py
@@ -3,8 +3,8 @@ from aoc_input_fetcher import fetch_input
 
 def main():
     data = fetch_input(__file__)
-    # data = '12345'
-    # data = '2333133121414131402'
+    # data = '12345\n'
+    # data = '2333133121414131402\n'
     print(part1(data))
     print(part2(data))
 
@@ -25,10 +25,6 @@ class CheckSumCalulator:
 
 
 def part1(data):
-    # free_space = 0
-    # for i, num in enumerate(data[:-1]):
-    #     if i%2 ==1:
-    #         free_space += int(num)
     data_m = data
     checksum = CheckSumCalulator()
     start = 0
@@ -37,39 +33,27 @@ def part1(data):
         start_num = int(data_m[start])
         if start%2 == 0:
             checksum.calculate_checksum(start_num, start//2)
-            start += 1
         else:
             nums_left = int(data_m[end])
             for _ in range(start_num):
                 if nums_left == 0:
+                    if end - 2 < start:
+                        break
                     end -= 2
                     nums_left = int(data_m[end])
+
                 checksum.calculate_checksum(1, end//2)
                 nums_left -= 1
-            data_m = data_m[:end] + str(nums_left) + data_m[end+1:]
+            data_m = data_m[:end] + str(nums_left)
+        start += 1
 
-
-            # for _ in range(start_num):
-            #     if nums_left == 0:
-                    
-            #         end -= 1
-            #         nums_left = int(data[end])
-            #     checksum += end/2*id
-            #     nums_left -= 1
-            #     id += 1
-    return checksum.get_checksum()      
-    for i, num in enumerate(data_m[:-1]):
-        if i%2 == 0:
-            # Possibly optimize with a sum formula?
-            for x in range(int(num)):
-                checksum=i*id
-                id += 1
-        else:
-            end = len(data_m) - 2
-            data_m[end]
-                
-
-    pass
+    while(start<len(data_m)):
+          start_num = int(data_m[start])
+          if start%2 == 0:
+            checksum.calculate_checksum(start_num, start//2)
+            start += 2
+          
+    return checksum.get_checksum()
 
 def part2(data):
     pass
