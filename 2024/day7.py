d91c85984977bf8f14f048b8cedf297c901cc592
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-12-13 17:27:54 -0800

    cleaning up

--- 2024/day7.py
+++ 2024/day7.py
@@ -22,24 +22,7 @@ def part1(data):
             total += eq[1]
     return total
 
-def maths_is_possible(eq):
-    nums, res = eq
-
-    def rec(nums, total):
-        if len(nums) == 0:
-            return total == res
-        return (rec(nums[1:], total=total+nums[0])) or rec(nums[1:], total*nums[0]) 
-
-    return rec(nums, 0)
-    
-def part2(data):
-    total = 0
-    for eq in data:
-        if maths_is_possible2(eq):
-            total += eq[1]
-    return total
-
-def maths_is_possible2(eq):
+def maths_is_possible(eq, allow_concat =False):
     nums, res = eq
 
     def rec(nums, total):
@@ -48,11 +31,19 @@ def maths_is_possible2(eq):
         n = nums[0]
         digits = 0
         while(n != 0):
-            rem = n% 10
             n //= 10 
             digits += 1
         multiplier = 10 ** digits
       
-        return rec(nums[1:], total=total+nums[0]) or rec(nums[1:], total*nums[0])  or rec(nums[1:],  (total) * multiplier + nums[0])
+        return rec(nums[1:], total=total+nums[0]) or rec(nums[1:], total*nums[0]) or (allow_concat and rec(nums[1:],  (total) * multiplier + nums[0])) 
+
     return rec(nums, 0)
+    
+def part2(data):
+    total = 0
+    for eq in data:
+        if maths_is_possible(eq, True):
+            total += eq[1]
+    return total
+
 main()
