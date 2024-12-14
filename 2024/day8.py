6683ccbce7e2d9ee3a2e1fda1e7f2882a3610888
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-12-13 19:07:03 -0800

    misunderstood the problem, gave the wrong solution

--- 2024/day8.py
+++ 2024/day8.py
@@ -2,18 +2,55 @@ from aoc_input_fetcher import fetch_input
 
 def main():
     data = fetch_input(__file__)
-    print(data)
     data = parse_data(data)
     print(part1(data))
     print(part2(data))
 
 def parse_data(data):
-    pass
+    return [[ant for ant in row] for row in data.splitlines()]
+
 
 def part1(data):
-    pass
-    
+    antinodes = set()
+
+    for i, row in enumerate(data):
+        for j, antenna in enumerate(row):
+            if antenna != '.':
+                antinodes.update( find_antinodes(i, j, data))
+    return len(antinodes)
+
+
+def find_antinodes(i, j, data):
+
+    antinodes = set()
+
+    for index in range(j+1, len(data)):
+        if data[i][j] == data[i][index]:
+            print("HOR")
+            dist = index - j
+            if index + dist < len(data):
+                antinodes.add((i, index + dist) )# 2in - j
+            if j - dist >= 0:
+                antinodes.add((i, j - dist)) # 2j - in
+    for index in range(i+1, len(data)):
+        if data[i][j] == data[index][j]:
+            dist = index - i
+            if index + dist < len(data):
+                antinodes.add((index + dist, j))
+            if i - dist >= 0:
+                antinodes.add((i - dist, j))
+    for dist in range(len(data) - max(i,j)):
+        if data[i][j] == data[i+dist][j+dist]:
+
+            if i - dist >= 0 and j - dist >= 0:
+                antinodes.add((i- dist, j- dist))
+            if i + (2*dist) < len(data) and j + (2*dist) < len(data):
+                antinodes.add((i + (2*dist), j + (2*dist)))
+            
+    return antinodes
+
 def part2(data):
     pass
+
 
 main()
